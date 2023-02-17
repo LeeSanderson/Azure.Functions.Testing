9f56c8522a4ee1183063fc5fd09a4da540c0a945
Lee Sanderson <lee.sndrsn@gmail.com>
2023-02-17 13:14:54 +0000

    Add debug code to changes script

--- CheckForSourceChanges.ps1
+++ CheckForSourceChanges.ps1
@@ -1,10 +1,12 @@
+
+write-host("Checking for changes in source code in $([Environment]::CurrentDirectory)")
+
 $sourceChanged = $false
 foreach ($_ in (git diff HEAD^ --name-only .\Src\Azure.Functions.Testing\)) 
 {
 	$sourceChanged = $true
 	break
 }
-
 if ($sourceChanged)
 {
 	write-host("Source code change detected, new NuGet package will be build")
